81f09988b66f61c0fd8e7e984356c70c9a184f1d
Maicon Diniz Filippsen <filippsen@blockie.org>
2016-12-26 01:25:49 +0100

    Update code documentation and module info details

--- Spacefile.sh
+++ Spacefile.sh
@@ -17,9 +17,15 @@
 # Disable warning about indirectly checking status code
 # shellcheck disable=SC2181
 
+#=====================
+# NETWORK_DEP_INSTALL
+#
+# Check dependencies for this module.
+#
+#=====================
 NETWORK_DEP_INSTALL ()
 {
-    SPACE_CMDDEP="OS_IS_INSTALLED PRINT"   # shellcheck disable=SC2034
+    SPACE_CMDDEP="OS_IS_INSTALLED PRINT"    # shellcheck disable=SC2034
     PRINT "Checking for OS dependencies." "info"
 
     OS_IS_INSTALLED "nmap" "nmap"
@@ -32,6 +38,18 @@ NETWORK_DEP_INSTALL ()
     fi
 }
 
+#=====================
+# NETWORK_DISCOVER
+#
+# Discover devices on a local network given an IP address range.
+#
+# Parameters:
+#   $1: IP address. Default: 192.168.0.0/24
+#
+# Returns:
+#   Non-zero on failure.
+#
+#=====================
 NETWORK_DISCOVER ()
 {
     SPACE_SIGNATURE="address"   # shellcheck disable=SC2034
@@ -45,6 +63,18 @@ NETWORK_DISCOVER ()
     nmap -sL "${address}" | grep '(.*)'
 }
 
+#=====================
+# NETWORK_PORTSCAN
+#
+# Scan for open ports on specific IP address.
+#
+# Parameters:
+#   $1: IP address. Default: 192.168.0.1
+#
+# Returns:
+#   Non-zero on failure.
+#
+#=====================
 NETWORK_PORTSCAN ()
 {
     SPACE_SIGNATURE="address"       # shellcheck disable=SC2034
@@ -59,3 +89,4 @@ NETWORK_PORTSCAN ()
     local SUDO="${SUDO-}"
     ${SUDO} nmap -sS "${address}"
 }
+
